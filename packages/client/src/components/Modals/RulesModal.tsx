f1b3d59fa806ddda2456fd2d2265f32402cdce16
ECWireless <elliott@coopallc.com>
2024-02-15 07:08:41 -0700

    Update rules to explain personal cards

--- packages/client/src/components/Modals/RulesModal.tsx
+++ packages/client/src/components/Modals/RulesModal.tsx
@@ -26,9 +26,9 @@ export const RulesModal: React.FC<RulesModalProps> = ({ isOpen, onClose }) => {
         </ModalHeader>
         <ModalBody>
           <Text>
-            Defeat Moloch! There are 3 moloch soldiers roaming the map. You can
-            go fast by playing alone, or you can form a raid party with other
-            characters, and go far.
+            Defeat Moloch! There are 4 moloch soldiers roaming the map. You can
+            go fast by playing alone, or you can form a raid party (by trading
+            cards) with other players, and go far.
           </Text>
           <Text fontSize="lg" fontWeight={500} mt={8}>
             Selecting a Class
@@ -52,17 +52,21 @@ export const RulesModal: React.FC<RulesModalProps> = ({ isOpen, onClose }) => {
             a new class for that card.
           </Text>
           <Text fontSize="sm" mt={2}>
-            Note that you must have at least 1 of your original cards (your
-            &quot;primary&quot; card) in your party at all times.
+            Note that you start with 3 personal cards (the card representing
+            your character) that you can trade with 2 other players. You must
+            have at least 1 of these personal cards in your party at all times,
+            and no player can hold 2 of the same cards (except for their own
+            personal cards).
           </Text>
           <Text fontSize="lg" fontWeight={500} mt={8}>
             Battling Moloch
           </Text>
           <Text fontSize="sm" mt={2}>
-            To battle a Moloch Soldier, make sure you are spawned on the map,
-            then walk up to a living moloch and hit &quot;enter&quot; or
-            &quot;e.&quot; If you run from battle or die, you&apos;ll reappear
-            on the map, where you can re-battle the Moloch Soldier at any time.
+            To battle a Moloch Soldier, make sure you are spawned on the map
+            (click the map after logging in), then walk up to a living moloch
+            and hit &quot;enter&quot; or &quot;e.&quot; If you run from battle
+            or die, you&apos;ll reappear on the map, where you can re-battle the
+            Moloch Soldier at any time.
           </Text>
           <Text fontSize="sm" mt={2}>
             Beating a Moloch Soldier grants you a place on the leaderboard. Note
